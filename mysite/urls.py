2c6b7ce6db9a21badc5971a3e6aa09c463ebe7e5
gnemoug <gnemoug@gmail.com>
2013-04-24 14:25:17 +0800

    complete

---
+++ mysite/urls.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
